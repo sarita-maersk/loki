f42da7befb9783e50c32a0c1cfc7ce3668108361
sarita-maersk <135721969+sarita-maersk@users.noreply.github.com>
2024-04-05 01:46:29 +0530

    log message change

--- clients/pkg/promtail/targets/syslog/syslogparser/syslogparser.go
+++ clients/pkg/promtail/targets/syslog/syslogparser/syslogparser.go
@@ -2,9 +2,13 @@ package syslogparser
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 
+	"github.com/go-kit/log/level"
+
+	"github.com/grafana/loki/pkg/util/log"
 	"github.com/influxdata/go-syslog/v3"
 	"github.com/influxdata/go-syslog/v3/nontransparent"
 	"github.com/influxdata/go-syslog/v3/octetcounting"
@@ -15,8 +19,16 @@ import (
 // detects octet counting.
 // The function returns on EOF or unrecoverable errors.
 func ParseStream(r io.Reader, callback func(res *syslog.Result), maxMessageLength int) error {
-	buf := bufio.NewReaderSize(r, 1<<10)
+	var buffer bytes.Buffer
+	bytesCopied, err := io.Copy(&buffer, r)
+	if err != nil {
+		return err
+	}
+	level.Info(log.Logger).Log("msg", "syslog stream reading", buffer.String(), "total bytes", bytesCopied)
 
+	newReader := bytes.NewReader(buffer.Bytes())
+
+	buf := bufio.NewReaderSize(newReader, 1<<10)
 	b, err := buf.ReadByte()
 	if err != nil {
 		return err
